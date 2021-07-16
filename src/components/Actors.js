8c91cd3904202dd8d588d371dbcc25a38a894644
abduallahB <abduallah.imu2@gmail.com>
2021-07-17 00:22:41 +0300

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,8 +1,28 @@
 import React from "react";
 import { actors } from "../data";
 
+
+
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+
+  const actorsList = actors.map((actor) => (
+    <div key={actor.name}>
+      <h3> {actor.name}</h3>
+      <ul>
+        {actor.movies.map((movie) => (
+          <li key={movie}>{movie} </li>
+        ))}
+      </ul>
+    </div>
+ ))
+
+
+  return (
+    <div>
+      <h1>Actors page </h1>
+      {actorsList}
+    </div>
+  );
 }
 
 export default Actors;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,19 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-
+// import { Navbar, Container, Nav } from 'react-bootstrap';
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div className="navbar">
+    <ul>
+
+      <li>   <NavLink to='/Home' >Home </NavLink> </li>
+      <li>   <NavLink to='/Movies' >Movies </NavLink> </li>
+      <li>   <NavLink to='/Actors' >Actors</NavLink> </li>
+      <li>   <NavLink to='/Directors' >Directors </NavLink> </li>
+
+    </ul>
+    </div>
+  );
 }
 
 export default NavBar;
